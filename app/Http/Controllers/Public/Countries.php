9013302f4290181e18af81a546237f7e2fe2d764
adverich <facu.ft@gmail.com>
2021-04-29 00:53:12 -0300

    updates

---
+++ app/Http/Controllers/Public/Countries.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Public;
+use App\Models\Public\Country;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Countries extends Controller {
+    public function index() {
+        return Country::get();
+    }
+}
