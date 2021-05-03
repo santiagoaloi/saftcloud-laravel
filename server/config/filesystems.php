240254d990b532dd5e155e42cde5d0f1ca1ae0f7
adverich <facu.ft@gmail.com>
2021-05-03 09:22:54 -0300

    updates

--- server/config/filesystems.php
+++ server/config/filesystems.php
@@ -66,7 +66,7 @@ return [
     */
 
     'links' => [
-        base_path('/public/storage') => storage_path('app/public'),
-    ],y
+        base_path('/../public/storage') => storage_path('app/public'),
+    ],
 
 ];
--- server/webpack.mix.js
+++ server/webpack.mix.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs-extra");
 const mix = require("laravel-mix");
 const { VuetifyLoaderPlugin } = require("vuetify-loader");
-const publicDir = "C:/xampp/htdocs/saftcloud-laravel/public";
+const publicDir = "C:/xampp/htdocs/laravel-vue-test/public";
 
 /*
 |---------------------------------------------------------------------
@@ -79,7 +79,7 @@ mix.js("resources/js/app.js", "public/dist/js")
         },
         output: {
             chunkFilename: "dist/js/[name].js",
-            path: "C:/xampp/htdocs/saftcloud-laravel/public/build",
+            path: "C:/xampp/htdocs/laravel-vue-test/public/build",
             // publicPath: process.env.APP_URL
             publicPath: mix.inProduction() ? process.env.APP_URL : undefined
         }
