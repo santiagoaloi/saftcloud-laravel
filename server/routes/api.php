bce990b3b3eea23f47ed139bcc658eeb2e135eab
adverich <facu.ft@gmail.com>
2021-05-16 12:27:05 -0300

    ups

--- server/routes/api.php
+++ server/routes/api.php
@@ -34,6 +34,6 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
