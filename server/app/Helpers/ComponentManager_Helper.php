c32e4cf16506d2bd465e430f5646cfc5eca0f7cd
adverich <facu.ft@gmail.com>
2021-09-14 20:22:05 -0300

    ups

--- server/app/Helpers/ComponentManager_Helper.php
+++ server/app/Helpers/ComponentManager_Helper.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Helpers;
 
+use Illuminate\Support\Facades\DB;
+
 class ComponentManager {
 
     static function parseComponent($component){
@@ -9,6 +11,10 @@ class ComponentManager {
         $config = ComponentManager::constructConfig($component->config);
         $configSettings = ComponentManager::constructConfig($component->config_settings);
         $status = ComponentManager::constructConfig($component->status);
+
+        // str_replace('SELECT', 'SELECT count(*) as temp, ', $sql_query);
+        $query = "SELECT * FROM capabilities WHERE name LIKE '$name.%'";
+        $capabilities = DB::SELECT($query);
 
         $origin = [
             'id'                => $component->id,
@@ -20,6 +26,7 @@ class ComponentManager {
             'created_at'        => $component->created_at,
             'updated_at'        => $component->updated_at,
             'deleted_at'        => $component->deleted_at,
+            'capabilities'      => $capabilities
         ];
 
         $result = [
@@ -33,6 +40,7 @@ class ComponentManager {
             'created_at'        => $component->created_at,
             'updated_at'        => $component->updated_at,
             'deleted_at'        => $component->deleted_at,
+            'capabilities'      => $capabilities
         ];
         return $result;
     }
