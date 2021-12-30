d0771f48c91d5a0bd0cb62b9de44d3683c4b54a1
Santiago Aloi <santiagoaloi@gmail.com>
2021-12-30 05:59:26 +0100

    deleting remote plugins folder

--- server/resources/js/plugins/vue-diagonal.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue';
-import VueDiagonal from 'vue-diagonal';
-
-Vue.component('VueDiagonal', VueDiagonal);
--- server/resources/js/plugins/vuex-pathify.js
+++
@@ -1,9 +0,0 @@
-// https://davestewart.github.io/vuex-pathify/#/setup/config
-
-import pathify from 'vuex-pathify';
-
-// options
-pathify.options.deep = 2;
-pathify.options.mapping = 'simple';
-
-export default pathify;
